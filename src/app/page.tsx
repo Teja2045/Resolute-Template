c07d00931d480f4d86771a47a14e1c564eae4e63
hemanthghs <ghshemanth@gmail.com>
2023-10-10 15:25:51 +0530

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import HomeComponent from "./components/HomeComponent";
 import Image from "next/image";
 import AccountDetails from "./components/AccountDetails";
+import TopNav from "./components/TopNav";
 
 const OverviewPage = () => {
   return (
     <div className="page">
+      <TopNav />
       <AccountDetails />
     </div>
   );
