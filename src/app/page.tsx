e2d73be65c99ec3ce0b8e7b740b462e72d64f854
pavania1 <adinapavani@gmail.com>
2023-10-11 10:10:50 +0530

    removed Walletpage in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,14 @@ import HomeComponent from "./components/HomeComponent";
 import Image from "next/image";
 import AccountDetails from "./components/AccountDetails";
 import TopNav from "./components/TopNav";
-import Walletpage from './components/WalletPage'
+
 
 const OverviewPage = () => {
   return (
     <div className="page">
       <TopNav />
       <AccountDetails />
-      <Walletpage />
+      
     </div>
   );
 };
