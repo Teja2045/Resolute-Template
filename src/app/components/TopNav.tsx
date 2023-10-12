5c34b82f9f05fe8572d51aa895e10dd5610be12e
hemanthghs <ghshemanth@gmail.com>
2023-10-12 16:30:54 +0530

    Add select network menu

--- src/app/components/TopNav.tsx
+++ src/app/components/TopNav.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import NetworksMenu from "./NetworksMenu";
 
 const TopNav = () => {
   return (
@@ -8,22 +9,7 @@ const TopNav = () => {
         <h2>Overview</h2>
       </div>
       <div className="topNav__options">
-        <div className="topNav__options__networks">
-          <div className="flex items-center gap-2">
-            <Image
-              src="./all-networks-icon.svg"
-              width={24}
-              height={24}
-              alt="All Networks"
-            />
-            <div className="topNav__options__networks__currentNetwork">
-              All Networks
-            </div>
-          </div>
-          <div>
-            <Image src='./down-arrow-icon.svg' width={24} height={24} alt="Select Network" />
-          </div>
-        </div>
+          <NetworksMenu />
         <div className="topNav__options__logout">
           <Image src="./logout-icon.svg" width={40} height={40} alt="Logout" />
         </div>
