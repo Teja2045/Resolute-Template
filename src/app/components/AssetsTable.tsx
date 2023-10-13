f7f69f161e7c5daa76754c0ed1ac05f167da51da
saiteja <tejargukt123@gmail.com>
2023-10-13 14:55:45 +0530

    changed table dimensions

--- src/app/components/AssetsTable.tsx
+++ src/app/components/AssetsTable.tsx
@@ -7,11 +7,11 @@ const AssetsTable = () => {
       <table className="customTable overflow-y-scroll">
         <thead className="customTableHead">
           <tr className="text-left">
-            <th>Network</th>
-            <th>StakeAmount</th>
-            <th>Rewards</th>
-            <th>Price</th>
-            <th>Actions</th>
+            <th className="w-1/4">Network</th>
+            <th className="w-1/6">StakeAmount</th>
+            <th className="w-1/6">Rewards</th>
+            <th className="w-1/6">Price</th>
+            <th className="">Actions</th>
           </tr>
         </thead>
         <tbody>
